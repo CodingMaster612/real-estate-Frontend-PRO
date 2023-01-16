df9e6a2448f8cb38981ec728c2989a7b1998dfc0
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-01-16 10:25:21 -0600

    fixed showing up scren

--- src/components/reusables/ViewBox.js
+++ src/components/reusables/ViewBox.js
@@ -1,59 +1,98 @@
-import axios from 'axios'
-import React, { useState ,useEffect } from 'react'
+// import axios from 'axios'
+// import React, { useState ,useEffect } from 'react'
+// import '../../css/reusables/viewProperty.css'
 
-function ViewBox() {
+// function ViewBox() {
 
 
-  const [property, setProperty] = useState({
-    contact: "",
-    images: "",
-    pasted: "",
-    price: "",
-    size: "",
-    location: "",
-    date: "",
-    discounted: ""
-  })
+//   const [property, setProperty] = useState({
+//     contact: "",
+//     images: "",
+//     pasted: "",
+//     price: "",
+//     size: "",
+//     location: "",
+//     date: "",
+//     discounted: ""
+//   })
   
   
-  useEffect(function(contact) {
+//   useEffect(function() {
    
     
-    axios.get(`http://localhost:8081/property/viewAll`)
-    .then(function(response) {
-        setProperty(response.data)
-    })
-    .catch((e) => {
-      console.log(e)
-    }) 
+//     axios.get(`http://localhost:8081/property/viewAll`)
+//     .then(function(response) {
+//       // const allProperty = response.data.property.allProperty;
+//       // setProperty(allProperty)
+//         setProperty(response.data)
+//     })
+//     .catch((e) => {
+//       console.log(e)
+//     }) 
 
-  }, [])
+//   }, [])
 
-    return (
-        
-        <header style={HeaderStyle}>
-          
+//   const displayData = () => {
+//     return property ? (
+//       property.map((property) => {
+//         return (
+//           <div className="data" key={property.id}>
+//             <h3>{property.image}</h3>
+//           </div>
+//         );
+//       })
+//     ) : (
+//       <h3>No data yet</h3>
+//     );
+//   }
+// return (
+//     <>
+//       {displayData()}
+//     </>
+//   );
+// }
 
-        
-            <h3>{property.contact}</h3>
-
-            
+// export default ViewBox
 
 
-
-
-
-
-        </header>
-    )
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+function ViewBox() {
+  const [property, setProperty] = useState("");
+const getAllProperty = () => {
+    axios
+      .get(`http://localhost:8081/property/viewAll`)
+      .then((response) => {
+        console.log(response.data);
+        setProperty(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+useEffect(() => {
+    getAllProperty();
+  }, []);
+const displayProperty = () => {
+    return property? (
+      property.map((property) => {
+        return (
+          <div className="data" key={property.id}>
+            <h3>{property.price}</h3>
+            <h3>{property.location}</h3>
+          </div>
+        );
+      })
+    ) : (
+      <h3>No data yet</h3>
+    );
+  }
+return (
+    <>
+      {displayProperty()}
+    </>
+  );
 }
-const HeaderStyle = {
-    width: "100%",
-    height: "100vh",
-    // background: `url(${BackgroundImage})`,
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
-    backgroundColor: "white"
-}
+
 export default ViewBox
