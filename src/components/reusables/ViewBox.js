fb75f55d73d9428f1b40c0072944cf86bbf7b50b
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2023-01-16 11:04:31 -0600

    fixed css screen

--- src/components/reusables/ViewBox.js
+++ src/components/reusables/ViewBox.js
@@ -1,66 +1,14 @@
-// import axios from 'axios'
-// import React, { useState ,useEffect } from 'react'
-// import '../../css/reusables/viewProperty.css'
 
-// function ViewBox() {
-
-
-//   const [property, setProperty] = useState({
-//     contact: "",
-//     images: "",
-//     pasted: "",
-//     price: "",
-//     size: "",
-//     location: "",
-//     date: "",
-//     discounted: ""
-//   })
-  
-  
-//   useEffect(function() {
-   
-    
-//     axios.get(`http://localhost:8081/property/viewAll`)
-//     .then(function(response) {
-//       // const allProperty = response.data.property.allProperty;
-//       // setProperty(allProperty)
-//         setProperty(response.data)
-//     })
-//     .catch((e) => {
-//       console.log(e)
-//     }) 
-
-//   }, [])
-
-//   const displayData = () => {
-//     return property ? (
-//       property.map((property) => {
-//         return (
-//           <div className="data" key={property.id}>
-//             <h3>{property.image}</h3>
-//           </div>
-//         );
-//       })
-//     ) : (
-//       <h3>No data yet</h3>
-//     );
-//   }
-// return (
-//     <>
-//       {displayData()}
-//     </>
-//   );
-// }
-
-// export default ViewBox
 
 
 import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import '../../css/reusables/viewProperty.css'
+
 function ViewBox() {
   const [property, setProperty] = useState("");
-const getAllProperty = () => {
+  const getAllProperty = () => {
     axios
       .get(`http://localhost:8081/property/viewAll`)
       .then((response) => {
@@ -71,24 +19,34 @@ const getAllProperty = () => {
         console.log(error);
       });
   };
-useEffect(() => {
+  useEffect(() => {
     getAllProperty();
   }, []);
-const displayProperty = () => {
-    return property? (
+  const displayProperty = () => {
+    return property ? (
       property.map((property) => {
         return (
-          <div className="data" key={property.id}>
-            <h3>{property.price}</h3>
-            <h3>{property.location}</h3>
+          <div className="wrapper" key={property.id}>
+
+
+            <div>{property.price}</div>
+            <div>{property.posted}</div>
+            x<div>{property.location}</div>
+            <div>{property.size}</div>
+            <div>{property.discounted}</div>
+            <div>{property.contact}</div>
+            <div>{property.date}</div>
+            <div><img src={property.images} /></div>
+
           </div>
+
         );
       })
     ) : (
       <h3>No data yet</h3>
     );
   }
-return (
+  return (
     <>
       {displayProperty()}
     </>
